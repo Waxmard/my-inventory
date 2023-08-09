1c2090e4e8857b4e0cf596bc55e4a0dd5c520826
Maxwell Ward <mward@knackworks.com>
2023-08-09 13:55:29 -0400

    robustify settings and setup.sh

--- proj/settings.py
+++ proj/settings.py
@@ -1,7 +1,13 @@
+import glob
 import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path='.env')
+env_override_files = glob.glob('.env.dev*')
+
+for env in env_override_files:
+    if os.path.exists(env):
+        load_dotenv(dotenv_path=env, override=True)
 
 DB_NAME = os.getenv('DB_NAME')
--- setup.sh
+++ setup.sh
@@ -4,3 +4,23 @@ echo "Setting up my-inventory project..."
 chmod +x my-inventory
 sudo ln -s "$(pwd)/my-inventory" /usr/local/bin
 echo "my-inventory command now runs project!"
+
+echo "Checking for Python3.11..."
+if ! command -v python3.11 &> /dev/null
+then
+    echo "Python3.11 is required. Please install it first."
+    exit 1
+fi
+
+echo "Setting up virtual environment..."
+python3.11 -m venv .venv
+
+echo "Installing required packages..."
+source .venv/bin/activate
+pip install --upgrade pip
+if [[ -f requirements.txt ]]; then
+    pip install -r requirements.txt
+fi
+
+echo "Virtual environment setup is complete."
+echo "To activate the virtual environment, run 'source .venv/bin/activate'"
